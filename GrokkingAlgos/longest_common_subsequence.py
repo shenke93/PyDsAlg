b735c4e964749029a8071fbb6854ace0f40fb2c3
KeShen <shen.ke1993@gmail.com>
2023-11-20 23:24:38 +0100

    add longest_common_subsequence

--- GrokkingAlgos/longest_common_subsequence.py
+++ GrokkingAlgos/longest_common_subsequence.py
@@ -1,6 +1,6 @@
-def longest_common_substring(word_a, word_b):
+def longest_common_subsequence(word_a, word_b):
     Cell = [[0 for x in range(len(word_a) + 1)] for x in range(len(word_b) + 1)]
-    max = 0
+
     # print(Cell)
     for i in range(len(word_b) + 1):
         for j in range(len(word_a) + 1):
@@ -8,13 +8,14 @@ def longest_common_substring(word_a, word_b):
                 Cell[i][j] = 0
             elif word_b[i-1] == word_a[j-1]:
                 Cell[i][j] = Cell[i-1][j-1] + 1
-                max = Cell[i][j]
             else:
-                Cell[i][j] = 0
+                Cell[i][j] = max(Cell[i-1][j], Cell[i][j-1])
     # print(Cell)
-    return max
+    return Cell[len(word_b)][len(word_a)]
+
 
 if __name__ == '__main__':
-    print(longest_common_substring('vista', 'hish'))
-    print(longest_common_substring('fosh', 'fish'))
-    print(longest_common_substring('fosh', 'fabc'))
+    print(longest_common_subsequence('vista', 'hish'))
+    print(longest_common_subsequence('fosh', 'fish'))
+    print(longest_common_subsequence('fosh', 'fabc'))
+    print(longest_common_subsequence('blue', 'clues'))
